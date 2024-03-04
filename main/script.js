15ab89b1d3559c27c59c4861f766f193dca97bda
Peterson Couto <142356426+petersonfernandocouto@users.noreply.github.com>
2024-03-04 14:49:11 -0300

    Book designer update

--- main/script.js
+++ main/script.js
@@ -32,3 +32,61 @@ function FormatoTempo( tempo ){
 
 countDown()
 setInterval(countDown, 1000)
+
+
+
+
+function VisualBox(selector){
+
+    var box = document.querySelector(selector);
+    box.classList.toggle("VisualBox");
+    
+}
+
+function compraIngresso(){
+
+    document.addEventListener('keydown', (event) => {
+
+        var ticket1 = document.getElementById("Thursday")
+        var ticket2 = document.getElementById("Friday")
+        var ticket3 = document.getElementById("Saturday")
+        var ticket4 = document.getElementById("Sunday")
+
+        var code = event.code;
+        if(code == "Digit1" || code == "Numpad1"){
+
+            ticket1.classList.toggle("colorTicket")
+            ticket2.classList.remove("colorTicket")
+            ticket3.classList.remove("colorTicket")
+            ticket4.classList.remove("colorTicket")
+        }
+
+        if(code == "Digit2" || code == "Numpad2"){
+
+            ticket1.classList.remove("colorTicket")
+            ticket2.classList.toggle("colorTicket")
+            ticket3.classList.remove("colorTicket")
+            ticket4.classList.remove("colorTicket")
+        }
+
+        if(code == "Digit3" || code == "Numpad3"){
+
+            ticket1.classList.remove("colorTicket")
+            ticket2.classList.remove("colorTicket")
+            ticket3.classList.toggle("colorTicket")
+            ticket4.classList.remove("colorTicket")
+        }
+
+        if(code == "Digit4" || code == "Numpad4"){
+
+            ticket1.classList.remove("colorTicket")
+            ticket2.classList.remove("colorTicket")
+            ticket3.classList.remove("colorTicket")
+            ticket4.classList.toggle("colorTicket")
+        }
+    }, false)
+}
+
+
+
+compraIngresso();
